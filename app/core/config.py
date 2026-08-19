aa4612b7db7fa2cc377514aa719f65a0a201f0e8
Freddy Guarachi <odiseo2712@hotmail.com>
2026-08-18 22:09:15 -0300

    Add migration alembic

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Setting(BaseSettings):
     DATABASE_URL: str
+    DATABASE_URL_SYNC: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
